1edbd2e38b3b6b239b113e52259b6dba16f00ca7
Tushar goyal <63923889+Novellino-tg@users.noreply.github.com>
2020-05-18 16:32:42 +0530

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,9 @@
+module.exports.help=function(){
+    console.log(`Commands to be used are:
+                    view=> To view files
+                    untreefy=>To create folders and files using the json file created from treefy command
+                    treefy  =>To create a json file with all the information about the folders and files in given src
+                    monitor =>To check details of latests changes in the files/folders
+                    help    =>To see commands which can be used in application`);
+
+};
